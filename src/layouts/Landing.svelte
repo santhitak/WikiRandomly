e5ec32b0ac495ba61405d25fe8be281b82cfa4b7
santhitak <santhita.sk@gmail.com>
2021-11-25 14:35:03 +0700

    setup: page layout

---
+++ src/layouts/Landing.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+
+</script>
+
+<div class="container">
+    <div class="contentWrapper">
+        <h2>Random Wikipedia</h2>
+    </div>
+</div>
+
+<style>
+    .container{
+        width: 100vw;
+        height: 100vh;
+        background-color: rgb(30, 37, 54);
+        color: rgb(235, 237, 245);
+    }
+    .contentWrapper{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
