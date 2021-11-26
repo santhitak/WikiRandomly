00f37241bd85dc7432d69ecee0bf9fc96017434d
santhitak <santhita.sk@gmail.com>
2021-11-26 12:18:49 +0700

    refactor: button in @components

--- src/layouts/Landing.svelte
+++ src/layouts/Landing.svelte
@@ -1,25 +1,48 @@
 <script lang="ts">
-
+  import ToggleBtn from "@components/toggleBtn.svelte";
 </script>
 
-<div class="container">
+<section>
+  <div class="container">
     <div class="contentWrapper">
-        <h2>Random Wikipedia</h2>
+      <h2>Random Wikipedia</h2>
+      <form action="">
+        <label for="wordSearch" />
+        <input type="text" class="search" value="" />
+      </form>
+      <ToggleBtn />
     </div>
-</div>
+
+  </div>
+</section>
 
 <style>
-    .container{
-        width: 100vw;
-        height: 100vh;
-        background-color: rgb(30, 37, 54);
-        color: rgb(235, 237, 245);
-    }
-    .contentWrapper{
-        width: 100%;
-        height: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+  .container {
+    width: 100vw;
+    height: 100vh;
+    background-color: var(--light);
+    color: var(--dark);
+    display: flex;
+    justify-content: center;
+  }
+  .contentWrapper {
+    width: 85%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+  .search{
+    padding: .5rem;
+    width: 16rem;
+  }
+
+  .search::selection {
+    font-weight: 600;
+    font-family: "Open Sans", sans-serif;
+    color: var(--light);
+    background: var(--dark);
+  }
 </style>
