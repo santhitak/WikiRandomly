f593ac6447059ae6d37633ea950e42e1ece94f1b
santhitak <santhita.sk@gmail.com>
2021-11-26 20:46:17 +0700

    add: landing layout

--- src/layouts/Landing.svelte
+++ src/layouts/Landing.svelte
@@ -1,18 +1,30 @@
 <script lang="ts">
-  import ToggleBtn from "@components/toggleBtn.svelte";
+  import Displayer from "@components/displayer.svelte";
 </script>
 
 <section>
   <div class="container">
     <div class="contentWrapper">
-      <h2>Random Wikipedia</h2>
-      <form action="">
-        <label for="wordSearch" />
-        <input type="text" class="search" value="" />
-      </form>
-      <ToggleBtn />
+      <div class="searchWrapper">
+        <h2>Random Wikipedia</h2>
+        <form action="">
+          <label for="wordSearch" />
+          <input
+            type="text"
+            class="search"
+            value=""
+            placeholder="type something"
+          />
+        </form>
+        <div>
+          <button class="custom_btn">SEARCH</button>
+          <button class="custom_btn">RANDOM</button>
+        </div>
+      </div>
     </div>
-
+    <div class="contentWrapper">
+      <Displayer />
+    </div>
   </div>
 </section>
 
@@ -22,20 +34,28 @@
     height: 100vh;
     background-color: var(--light);
     color: var(--dark);
-    display: flex;
+    display: grid;
     justify-content: center;
+    grid-template-columns: 25vw 75vw;
   }
   .contentWrapper {
-    width: 85%;
+    width: 100%;
     height: 100%;
     display: flex;
-    align-items: center;
     justify-content: center;
-    flex-direction: column;
+    align-items: center;
   }
 
-  .search{
-    padding: .5rem;
+  .searchWrapper {
+    height: 40vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  .search {
+    padding: 0.5rem;
     width: 16rem;
   }
 
