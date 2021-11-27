c2d7fe6fb61f88045db0ccc0b9424ce8a6256b76
santhitak <santhita.sk@gmail.com>
2021-11-27 23:56:50 +0700

    add: new logo

---
+++ src/assets/logo.svelte
@@ -0,0 +1,34 @@
+<div class="logoContain">
+  <p id="icon">RM</p>
+  <p id="text">Random<br />Wikipedia</p>
+</div>
+
+<style>
+  #icon {
+    text-shadow: none;
+    font-size: 1.5rem;
+    color: rgb(255, 255, 255);
+    height: 65px;
+    width: 65px;
+    border-radius: 20px;
+    font-weight: 700;
+    text-align: center;
+    background-color: rgb(0, 0, 0);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  #text {
+    font-weight: 700;
+    font-size: 1.3rem;
+    line-height: 100%;
+  }
+  .logoContain {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  p {
+    padding: 1rem;
+  }
+</style>
