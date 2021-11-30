8fefd571166c56bab7813f1ebcd5cf053375e932
santhitak <santhita.sk@gmail.com>
2021-11-30 12:40:49 +0700

    style: logo

--- src/assets/logo.svelte
+++ src/assets/logo.svelte
@@ -1,16 +1,15 @@
 <div class="logoContain">
   <p id="icon">RM</p>
-  <p id="text">Random<br />Wikipedia</p>
 </div>
 
 <style>
   #icon {
     text-shadow: none;
-    font-size: 1.5rem;
+    font-size: 1.2rem;
     color: rgb(255, 255, 255);
-    height: 65px;
-    width: 65px;
-    border-radius: 20px;
+    height: 50px;
+    width: 50px;
+    border-radius: 15%;
     font-weight: 700;
     text-align: center;
     background-color: rgb(0, 0, 0);
@@ -18,17 +17,10 @@
     align-items: center;
     justify-content: center;
   }
-  #text {
-    font-weight: 700;
-    font-size: 1.3rem;
-    line-height: 100%;
-  }
   .logoContain {
     display: flex;
     align-items: center;
     justify-content: center;
-  }
-  p {
-    padding: 1rem;
+    user-select: none;
   }
 </style>
